df2099193a00e638e8d43e897d8b62fdda35a69e
GD <gautier.dulac@polytechnique.Edu>
2020-02-18 14:50:33 +0100

    UPDATE - Loading primers depending on the ressource of litterature used

--- loading/loading_primers.py
+++ loading/loading_primers.py
@@ -2,19 +2,22 @@
 Loading functions to load primers from fasta files
 """
 
-from utils.utils import folder_paths
-import pandas as pd
 from typing import Dict
+
+import pandas as pd
+
+from utils.utils import folder_paths
 
 
 # Load primers fasta data already saved
-def load_primers(forward_or_reverse: str = '') -> pd.DataFrame:
+def load_primers(forward_or_reverse: str = '', article: str = '') -> pd.DataFrame:
     """
     Load primers from data fasta file
+    :param article: Where to look for primers (depending on the ressource used to look for it)
     :param forward_or_reverse: string telling if forward or reverse primers are to be loaded
     :return:
     """
-    seq_file = open(folder_paths['data'] + '{}_primers.fasta'.format(forward_or_reverse))
+    seq_file = open(folder_paths[article] + '{}_primers.fasta'.format(forward_or_reverse))
     line_to_read = True
     list_of_reads = []
     while line_to_read:
@@ -28,18 +31,17 @@ def load_primers(forward_or_reverse: str = '') -> pd.DataFrame:
     return pd.DataFrame(list_of_reads, columns=['hvr', 'sequence', 'forward_or_reverse'])
 
 
-def get_dict_of_primers(start_df: pd.DataFrame = load_primers('forward'),
-                        end_df: pd.DataFrame = load_primers('reverse')) -> \
-        Dict[str, Dict[str, str]]:
+def get_dict_of_primers(article: str = '') -> Dict[str, Dict[str, str]]:
     """
     Return a dict format with keys as hvr, and a dict with start and end keys for each hvr and the relevant sequence
-    :param start_df: forward_primers df
-    :param end_df: reverse_primers df
+    :param article: where to look for primers (depending on the ressource used to look for it)
     :return: Dict format
     """
+    forward_df = load_primers(forward_or_reverse='forward', article=article)
+    reverse_df = load_primers(forward_or_reverse='reverse', article=article)
     primers_dict = {}
-    for i in range(len(start_df.hvr)):
-        hvr = start_df.hvr[i]
-        primers_dict[hvr] = {'forward': start_df.loc[start_df.hvr == hvr].sequence[i],
-                             'reverse': end_df.loc[end_df.hvr == hvr].sequence[i]}
+    for i in range(len(forward_df.hvr)):
+        hvr = forward_df.hvr[i]
+        primers_dict[hvr] = {'forward': forward_df.loc[forward_df.hvr == hvr].sequence[i],
+                             'reverse': reverse_df.loc[reverse_df.hvr == hvr].sequence[i]}
     return primers_dict
