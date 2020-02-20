72ce6d29f6c1fe38b69ea71d2ced064533ac3ea9
GD <gautier.dulac@polytechnique.Edu>
2020-02-20 11:19:59 +0100

    ADD - Better hvr extraction (removing size 0 hvr) ANALYSES - HVR using DB primers on Dairy db and GG db

--- processing/hvr_extraction.py
+++ processing/hvr_extraction.py
@@ -10,28 +10,31 @@ import pandas as pd
 from loading.loading_primers import get_dict_of_primers
 from utils.utils import get_list_of_related_primers, get_searchable_reverse_primer, folder_paths
 
+
 # Constant
-dict_of_primers = get_dict_of_primers(article='DairyDB')
 
 
 # Main Function
-def get_all_hvr(sequence_df: pd.DataFrame, save_name: str = None) -> pd.DataFrame:
+def get_all_hvr(sequence_df: pd.DataFrame, primers_origin: str = 'DairyDB', save_name: str = None) -> pd.DataFrame:
     """
     for a given dataframe of sequences, extract all hvr defined by primers in dict_of_primers above.
     :return: a dataframe with column being hvr and values being subsequences of hvr or None, an index column is kept as
     the one in the sequence_df
     """
+    dict_of_primers = get_dict_of_primers(article=primers_origin)
     hvr_df = pd.DataFrame()
     work_size = len(sequence_df)
     percent = work_size // 100
-    for index in sequence_df.index:
-        if index % percent == 0:
-            print('Achieved extraction: {}%'.format(np.round(index // percent, 2)), end='\r')
+    for i in range(len(sequence_df)):
+        index = list(sequence_df.index)[i]
+        if i % percent == 0:
+            print('Achieved extraction: {}%'.format(np.round(i // percent, 2)), end='\r')
         current_seq = sequence_df.sequence[index]
-        positions_dict = get_all_hvr_positions_by_sequence(current_seq)
+        positions_dict = get_all_hvr_positions_by_sequence(current_seq, dict_of_primers=dict_of_primers)
         current_hvr_dict = {"index": [index]}
         for key, value in positions_dict.items():
-            if value is None:
+            # Not found HVR or forward primer after reverse primer -> None
+            if value is None or value[0][0] > value[0][1]:
                 current_hvr_dict[key] = None
             else:
                 current_hvr_dict[key] = current_seq[value[0][0]:value[0][1]]
@@ -43,22 +46,25 @@ def get_all_hvr(sequence_df: pd.DataFrame, save_name: str = None) -> pd.DataFram
 
 
 # Functions
-def get_all_hvr_positions_by_sequence(current_seq: str) -> Dict[str, Union[List[Tuple[int, int]], None]]:
+def get_all_hvr_positions_by_sequence(current_seq: str,
+                                      dict_of_primers: dict) -> Dict[str, Union[List[Tuple[int, int]], None]]:
     """
     For a given sequence and hvr to look for, return the dictionnary with hvr as keys and list (just a element if one)
     with starting and ending positions of the given hvr
+    :param dict_of_primers:
     :param current_seq: seq in which to look for
     :return: dictionnary with hvr as keys with starting and ending positions of the given hvr in a list
     """
     dict_of_observed_hvr = {}
     for hvr in dict_of_primers.keys():
-        dict_of_observed_hvr[hvr] = get_hvr_position(current_seq, hvr)
+        dict_of_observed_hvr[hvr] = get_hvr_position(current_seq, hvr, dict_of_primers)
     return dict_of_observed_hvr
 
 
-def get_hvr_position(sequence: str, hvr: str) -> Union[List[Tuple[int, int]], None]:
+def get_hvr_position(sequence: str, hvr: str, dict_of_primers: dict) -> Union[List[Tuple[int, int]], None]:
     """
     For a given sequence and hvr, look for the starting and ending points in the sequence, or return None
+    :param dict_of_primers:
     :param sequence: current sequence
     :param hvr: current hvr (has to be in dict_of_primers.keys())
     :return: A list with one element (a tuple (start, end)) or None
