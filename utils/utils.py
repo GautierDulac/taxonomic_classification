b477a7df700ab489bf82457f32fa97dda30629b0
GD <gautier.dulac@polytechnique.Edu>
2020-02-21 13:56:58 +0100

    ADD - model_data folder

--- utils/utils.py
+++ utils/utils.py
@@ -5,6 +5,7 @@ from typing import Union, List
 folder_paths = {'Sequence': 'D:\\0 - Boulot\\5 - X4\\16. Research Paper\\data\\gg_13_5_otus\\rep_set\\',
                 'Taxonomy': 'D:\\0 - Boulot\\5 - X4\\16. Research Paper\\data\\gg_13_5_otus\\taxonomy\\',
                 'data': 'D:\\0 - Boulot\\5 - X4\\16. Research Paper\\data\\',
+                'model_data': 'D:\\0 - Boulot\\5 - X4\\16. Research Paper\\model_data\\',
                 'Chaudhary': 'D:\\0 - Boulot\\5 - X4\\16. Research Paper\\data\\primers\\Chaudhary\\',
                 'DairyDB': 'D:\\0 - Boulot\\5 - X4\\16. Research Paper\\data\\primers\\DairyDB\\',
                 'stats_results': 'D:\\0 - Boulot\\5 - X4\\16. Research Paper\\taxonomic_classification\\results\\statistics\\'}
